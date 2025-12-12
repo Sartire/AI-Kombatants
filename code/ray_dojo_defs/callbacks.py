cb0c726174f9b6d108f25558a9df28376e3f734f
Sartire <alexvdeluca@gmail.com>
2025-12-12 17:18:38 -0500

    Update callbacks.py

--- code/ray_dojo_defs/callbacks.py
+++ code/ray_dojo_defs/callbacks.py
@@ -1,11 +1,10 @@
 from ray.rllib.callbacks.callbacks import RLlibCallback
-
+import numpy as np
 class EpisodeReturn(RLlibCallback):
-    def __init__(self):
-        super().__init__()
-        # Keep some global state in between individual callback events.
-        self.overall_sum_of_rewards = 0.0
+    
 
     def on_episode_end(self, *, episode, metrics_logger, **kwargs):
-        self.overall_sum_of_rewards += episode.get_return()
-        metrics_logger.log_value("episode_return", self.overall_sum_of_rewards)
+        
+        reward_total = np.sum(episode.get_rewards())
+        metrics_logger.log_value("episode_return", reward_total, reduce="mean", window=50)
+
